0b7f88632e9acc2d5f2d73b3420c205894a3ad97
Yousseif Elshahawy [z14] <yousseif.elshahawy@gmail.com>
2025-05-04 11:18:45 +0300

    better auth forms

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,14 +1,17 @@
 <template>
-    <UForm :schema @submit.prevent="signIn" :state class="w-full space-y-3">
+    <UForm :schema @submit.prevent="signIn" :state
+        class="w-full gap-5 bg-gray-50 p-12 rounded-lg shadow-md max-w-4xl mx-auto flex flex-col">
+        <h1 class="text-2xl font-bold mb-3">Login</h1>
+
         <UFormField label="Email" name="email">
-            <UInput v-model="state.email" />
+            <UInput v-model="state.email" class="w-full" />
         </UFormField>
 
         <UFormField label="Password" name="password">
-            <UInput v-model="state.password" />
+            <UInput type="password" v-model="state.password" class="w-full" />
         </UFormField>
 
-        <UButton loading-auto type="submit">Sign In</UButton>
+        <UButton loading-auto type="submit" class="self-end">Sign In</UButton>
     </UForm>
 </template>
 
@@ -18,8 +21,8 @@ import { z } from "zod";
 import type { FormSubmitEvent } from "@nuxt/ui";
 
 const schema = z.object({
-    email: z.string(),
-    password: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8),
 })
 
 type Schema = z.output<typeof schema>
@@ -34,10 +37,14 @@ const { client } = useAuth()
 const signIn = async (event: FormSubmitEvent<Schema>) => {
     let { email, password } = event.data
 
-    await client.signIn.email({
+    const response = await client.signIn.email({
         email: email,
         password: password,
     })
+
+    if (response.error) {
+        console.error(response.error)
+    }
 
     await navigateTo("/")
 };
--- app/pages/register.vue
+++ app/pages/register.vue
@@ -1,37 +1,39 @@
 <template>
-    <UForm :schema @submit.prevent="signUp" :state class="w-full space-y-3">
-        <UFormField label="First Name" name="firstName">
-            <UInput v-model="state.firstName" />
+    <UForm :schema @submit.prevent="signUp" :state
+        class="w-full gap-5 bg-gray-50 p-12 rounded-lg shadow-md max-w-4xl mx-auto flex flex-col">
+        <h1 class="text-2xl font-bold mb-3">Register</h1>
+        <UFormField label="First Name" name="firstName" required>
+            <UInput v-model="state.firstName" class="w-full" />
         </UFormField>
 
-        <UFormField label="Last Name" name="lastName">
-            <UInput v-model="state.lastName" />
+        <UFormField label="Last Name" name="lastName" required>
+            <UInput v-model="state.lastName" class="w-full" />
         </UFormField>
 
-        <UFormField label="Email" name="email">
-            <UInput v-model="state.email" />
+        <UFormField label="Email" name="email" required>
+            <UInput v-model="state.email" class="w-full" />
         </UFormField>
 
-        <UFormField label="Password" name="password">
-            <UInput v-model="state.password" />
+        <UFormField label="Password" name="password" required>
+            <UInput v-model="state.password" class="w-full" />
         </UFormField>
 
-        <UFormField label="Gender" name="gender">
-            <USelect v-model="state.gender" :items="['male', 'female']" />
+        <UFormField label="Gender" name="gender" required>
+            <USelect v-model="state.gender" :items="['male', 'female']" class="w-full" />
         </UFormField>
 
-        <UFormField label="Age" name="age">
-            <UInput type="number" v-model="state.age" />
+        <UFormField label="Age" name="age" required>
+            <UInput type="number" v-model="state.age" class="w-full" />
         </UFormField>
 
-        <UFormField label="Mother Country" name="motherCountry">
-            <UInput v-model="state.motherCountry" />
+        <UFormField label="Mother Country" name="motherCountry" required>
+            <UInput v-model="state.motherCountry" class="w-full" />
         </UFormField>
 
-        <UFormField label="Spoken Dialect" name="spokenDialect">
-            <UInput v-model="state.spokenDialect" />
+        <UFormField label="Spoken Dialect" name="spokenDialect" required>
+            <UInput v-model="state.spokenDialect" class="w-full" />
         </UFormField>
-        <UButton loading-auto type="submit">Sign Up</UButton>
+        <UButton loading-auto type="submit" class="self-end">Sign Up</UButton>
     </UForm>
 </template>
 
@@ -40,14 +42,14 @@ import { z } from "zod";
 import type { FormSubmitEvent } from "@nuxt/ui";
 
 const schema = z.object({
-    firstName: z.string(),
-    lastName: z.string(),
-    email: z.string(),
-    password: z.string(),
+    firstName: z.string().min(1),
+    lastName: z.string().min(1),
+    email: z.string().email(),
+    password: z.string().min(8),
     gender: z.string(),
-    age: z.number(),
-    motherCountry: z.string(),
-    spokenDialect: z.string(),
+    age: z.number().min(10).max(100),
+    motherCountry: z.string().min(1),
+    spokenDialect: z.string().min(1),
 })
 
 type Schema = z.output<typeof schema>
@@ -78,7 +80,7 @@ const signUp = async (event: FormSubmitEvent<Schema>) => {
     let motherCountry = event.data.motherCountry.toLowerCase()
     let spokenDialect = event.data.spokenDialect.toLowerCase()
 
-    await client.signUp.email({
+    const response = await client.signUp.email({
         firstName: firstName,
         lastName: lastName,
         name: name,
@@ -89,6 +91,10 @@ const signUp = async (event: FormSubmitEvent<Schema>) => {
         motherCountry: motherCountry,
         spokenDialect: spokenDialect,
     });
+
+    if (response.error) {
+        console.error(response.error)
+    }
 
     await navigateTo("/")
 };
